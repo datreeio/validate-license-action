43581752e5e82bf0961948c40d1cd5d5afa6e93f
Roman Labunsky <roman@datree.io>
2019-02-04 13:35:52 +0200

    fix: basic action

---
+++ validateLicense.js
@@ -0,0 +1,16 @@
+async function main() {
+  const event = JSON.parse(fs.readFileSync('/github/workflow/event.json', 'utf8'))
+  console.log(event)
+}
+
+if (require.main === module) {
+  main()
+    .then(res => {
+      console.log({ res })
+      process.exitCode = 0
+    })
+    .catch(err => {
+      console.log({ err })
+      process.exitCode = 1
+    })
+}
