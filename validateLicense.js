da62c03de124f4c13ac94e4d464e9200e66dcc05
Roman Labunsky <roman@datree.io>
2019-02-05 16:15:41 +0200

    fix: license validation

--- validateLicense.js
+++ validateLicense.js
@@ -7,12 +7,14 @@ program
 
 async function main() {
   const event = JSON.parse(fs.readFileSync('/github/workflow/event.json', 'utf8'))
-  const validLicenses = program.licenses
-  const repoLicense = event.repository.license
-  console.log(event)
-  console.log(process.argv)
-  console.log(validLicenses)
-  console.log(repoLicense)
+  const validLicenses = program.licenses.split(',').map(val => val.toLower())
+  let repoLicense = event.repository.license
+  if (repoLicense && validLicenses && validLicenses.includes(repoLicense)) return 'Valid license'
+  // console.log(event)
+  // console.log(process.argv)
+  // console.log(validLicenses)
+  // console.log(repoLicense)
+  throw new Error(`The license ${repoLicense} is not valid in this repo`)
 }
 
 if (require.main === module) {
